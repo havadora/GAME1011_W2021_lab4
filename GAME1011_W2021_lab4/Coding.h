a32ae5ad27c2c273538a425cbab3c1b738b89e69
havadora <71092442+havadora@users.noreply.github.com>
2021-03-18 19:40:01 -0400

    main

--- GAME1011_W2021_lab4/Coding.h
+++ GAME1011_W2021_lab4/Coding.h
@@ -1,16 +1,98 @@
 #pragma once
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+
 
 using namespace std;
 
 class ForString
 {
-
+public:
+	
+	void Reverse(char ch[], int MaxSize, int MinSize, string str)
+	{
+		char temp;
+			temp = ch[MinSize];
+			ch[MinSize] = ch[MaxSize];
+			ch[MaxSize] = temp;
+			
+			if ( MaxSize - MinSize == 1)
+			{
+				cout << "The reverse of " << str << ": ";
+				for (int i = 0; i < MaxSize; i++)
+				{
+					cout << ch[i];
+				}
+		    }
+			else if ( MaxSize - MinSize == 0)
+			{
+				cout << "The reverse of " << str << ": ";
+				for (int i = 0; i < MaxSize; i++)
+				{
+					cout << ch[i];
+				}
+			}
+			else
+			return Reverse(ch, MaxSize - 1, MinSize + 1, str);
+	}
+	int Compare(char ch[], int MaxSize, int MinSize)
+	{
+		if (ch[MinSize] == ch[MaxSize])
+		{
+			if (MaxSize - MinSize == 1)
+			{
+				return 0;
+			}
+			else
+			{
+				return Compare(ch, MaxSize - 1, MinSize + 1);
+			}
+		}
+		else
+			return -1;
+	}
 };
 
 class ForInteger
 {
+public:
+	int partition(int* a, int begin, int last)
+	{
+		int pivot = a[last];
+		int DivideNumber = begin;
+		int temp;
+		for (int i = begin; i < last; i++)
+		{
+			if (a[i] <= pivot)
+			{
+				temp = a[i];
+				a[i] = a[DivideNumber];
+				a[DivideNumber] = temp;
+				DivideNumber++;
+			}
+		}
+		temp = a[last];
+		a[last] = a[DivideNumber];
+		a[DivideNumber] = temp;
 
+		return DivideNumber;
+	}
+	void Quicksort(int* a, int begin, int last)
+	{
+		if (begin < last)
+		{
+			int P_index = partition(a, begin, last);
+			Quicksort(a, begin, P_index - 1);
+			Quicksort(a, P_index + 1, last);
+		}	
+	}
+	void display(int* a, int last)
+	{
+		cout << "\nThe sorting array: ";
+		for (int i = 0; i < last; i++)
+		{
+			cout << a[i];
+			cout << " ";
+		}
+	}
+	
 };
---
+++ GAME1011_W2021_lab4/Main.cpp
@@ -0,0 +1,86 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Coding.h"
+
+using namespace std;
+
+int main()
+{
+	bool conti= true;
+	int opt;
+	
+	ForString str;
+	ForInteger inter;
+
+	cout << "Welcome to the Lab 4 code " << endl;
+	while (conti == true )
+	{
+
+		cout << "\n1. Checking palindrome" << endl;
+		cout << "2. Quick sorting " << endl;
+		cout << "3. Reverse string " << endl;
+		cout << "4. Exit " << endl;
+		cout << "Choose 1-4 options above : " << endl;
+		cin >> opt;
+		switch (opt)
+		{
+		case 1:
+		{
+			string strr;
+			cout << "Enter the word: ";
+			cin >> strr;
+			char ar[sizeof(strr) + 1];
+			for (int i = 0; i < sizeof(ar); i++) {
+				ar[i] = strr[i];
+			}
+			if (str.Compare(ar, sizeof(ar), 0) == -1)
+			{
+				cout << "This is not palindrome" << endl;
+			}
+			if (str.Compare(ar, sizeof(ar), 0) == 0)
+			{
+				cout << "This is palindrome" << endl;
+			}
+			cout << "";
+			break;
+		}
+		case 2:
+		{
+			srand(unsigned(time(NULL)));
+			int num;
+			cout << "Enter the size of array: ";
+			cin >> num;
+			int* arn = new int[num];
+			for (int i = 0; i < num; i++)
+			{
+				arn[i] = rand() % 100;
+			}
+			inter.display(arn, num);
+			inter.Quicksort(arn, 0, num - 1);
+			inter.display(arn, num);
+			delete arn;
+			cout << "";
+			break;
+		}
+		case 3:
+		{
+			string strr;
+			cout << "Enter the word: ";
+			cin >> strr;
+			char arr[sizeof(strr) + 1];
+			for (int i = 0; i < sizeof(arr); i++)
+			{
+				arr[i] = strr[i];
+			}
+			str.Reverse(arr, sizeof(arr), 0, strr);
+			cout << "";
+			break;
+		}
+		case 4:
+			conti = false;
+		}
+	}
+	system("pause");
+}
